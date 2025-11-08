1e9bdd02b76c66f9fc1370ad9afa6fdda71d7ad9
adriano-camargos <adriano.camargos@hotmail.com>
2025-11-08 18:49:51 -0300

    Adicionando domínio do PythonAnywhere aos hosts permitidos

--- config/settings.py
+++ config/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = '...' # (a sua chave secreta estará aqui, não a modifique)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'adrianocamargos.pythonanywhere.com']
 
 # Application definition
 
